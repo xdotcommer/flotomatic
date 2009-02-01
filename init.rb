8c7c5946f80fd8214f1dc16a53ce9540292745c7
Michael Cowden <mcowden@yahoo.com>
2009-02-01 12:25:45 -0600

    getting rid of flotomatic.rb and moving stuff into init.rb

--- init.rb
+++ init.rb
@@ -1,1 +1,6 @@
 # Include hook code here
+require 'flot'
+require 'time_flot'
+
+$LOAD_PATH << File.join(directory, 'app', 'helpers')
+ActiveSupport::Dependencies.load_paths << File.join(directory, 'app', 'helpers')
--- lib/flotomatic.rb
+++
@@ -1,2 +0,0 @@
-# Flotomatic
-require 'flot'
