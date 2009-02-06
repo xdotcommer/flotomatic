86425022d824b05875757ba8424aba663610f4ca
Michael Cowden <mcowden@yahoo.com>
2009-02-06 06:32:45 -0600

    Really... don't remove their jquery and flot js files, it's not right...

--- uninstall.rb
+++ uninstall.rb
@@ -5,7 +5,9 @@ directory = File.dirname(__FILE__)
 FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "flotomatic.js")
 FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "stylesheets", "flotomatic.css")
 
-FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "excanvas.pack.js")
-FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "jquery.js")
-FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "jquery-ui.js")
-FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "jquery.flot.pack.js")
+# Really?  Are you absolutely sure?
+# Better safe than sorry, imho
+# FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "excanvas.pack.js")
+# FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "jquery.js")
+# FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "jquery-ui.js")
+# FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "jquery.flot.pack.js")
