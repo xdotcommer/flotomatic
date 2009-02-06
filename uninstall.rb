e91332748ea70a5d6fdab747fa5db8b742301559
Michael Cowden <mcowden@yahoo.com>
2009-02-06 06:14:01 -0600

    Cleaning up after yourself is good: get rid of public files on uninstall

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,6 @@
-# Uninstall hook code here
+require 'fileutils'
+
+directory = File.dirname(__FILE__)
+
+FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "javascripts", "flotomatic.js")
+FileUtils.rm_r File.join(directory, "..", "..", "..", "public", "stylesheets", "flotomatic.css")
