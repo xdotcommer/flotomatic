c63861e60cc704a23c794dee67f87c96bd922865
xdotcommer <mcowden@yahoo.com>
2009-12-02 13:47:44 -0700

    Quick fix to make things work with Rails 2.3

--- init.rb
+++ init.rb
@@ -1,5 +1,9 @@
 require 'flot'
 require 'time_flot'
 
-$LOAD_PATH << File.join(directory, 'app', 'helpers')
-ActiveSupport::Dependencies.load_paths << File.join(directory, 'app', 'helpers')
+if RAILS_GEM_VERSION.split('.')[0] == '2' && RAILS_GEM_VERSION.split('.')[1] == '3'
+  ActionView::Base.send :include, FlotHelper
+else
+  $LOAD_PATH << File.join(directory, 'app', 'helpers')
+  ActiveSupport::Dependencies.load_paths << File.join(directory, 'app', 'helpers')
+end
